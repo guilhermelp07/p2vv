8b3e98a01faf3dc5738befc3b706594431418602
guilhermelp07 <guilpissaia@hotmail.com>
2023-06-25 23:18:08 -0300

    ajuste na config para funcionar no chrome

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
   e2e: {
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
